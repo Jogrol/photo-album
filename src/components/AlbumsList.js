a215dc6141cce41f6ef0fe9bc60737f6d236f0c3
Joey Grolleman <joeygrolleman@gmail.com>
2019-01-25 17:39:20 +0100

    set photo album

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,11 +1,14 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function AlbumsList (props) {
 
   return (<div>
     <h1>All Albums</h1>
     {props.albums.map((album) => 
-      <li key={album.id}>{album.title}</li>)}
+      <li key={album.id}>
+      <Link to = {`/albums/${album.id}`}>{album.title}</Link>
+      </li>)}
 
     There are { props.albums.length } albums available.
   </div>)
