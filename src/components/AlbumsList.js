c1b32b16efdac3c75b7ce302fee07d9a41aa126f
Joey Grolleman <joeygrolleman@gmail.com>
2019-01-25 12:08:07 +0100

    set up fetch albums

---
+++ src/components/AlbumsList.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export default function AlbumsList(props) {
+
+  return (<div>
+    <h1>All Albums</h1>
+    {props.albums.map((album) => 
+      <li key={album.id}>{album.title}</li>)}
+
+    There are { props.albums.length } albums available.
+  </div>)
+}
