d63403e881319f61d686ffe8ee85befff3174303
Joey Grolleman <joeygrolleman@gmail.com>
2019-01-25 15:47:06 +0100

    Async actions

--- src/components/AlbumsListContainer.js
+++ src/components/AlbumsListContainer.js
@@ -1,18 +1,48 @@
 import * as React from 'react'
 import * as request from 'superagent'
 import AlbumsList from './AlbumsList'
+import { connect } from 'react-redux'
+import { helloWorld } from '../actions/test'
+import { addAlbum } from '../actions/albums'
 
-export default class AlbumsListContainer extends React.Component {
+
+const sleep = time => new Promise(
+  resolve => setTimeout(() => resolve(`I waited for ${time} ms`), time)
+)
+
+
+
+class AlbumsListContainer extends React.Component {
   state = {}
 
-  componentDidMount() {
-    request('https://jsonplaceholder.typicode.com/albums')
-      .then(response => this.setState({ albums: response.body }))
-  }
+
+
+  componentDidMount() 
+  // {
+  //   request('https://jsonplaceholder.typicode.com/albums')
+  //     .then(response => response.body.map(album => this.props.addAlbum(album.id, album.title)))
+  //   // set this.props instead of setstate().
+  // }
+  {
+  sleep(2000)
+    .then(message => this.props.addAlbum(1, message))
+
+  sleep(3000)
+    .then(message => this.props.addAlbum(2, message))
+}
+  
 
 
   render() {
-    if (!this.state.albums) return 'Loading...'
-    return <AlbumsList albums={this.state.albums} />
+    if (!this.props.albums) return 'Loading...'
+    return <AlbumsList albums={this.props.albums} />
+    
   }
 }
+
+const mapStateToProps = (state) => {
+    return  {
+      albums: state.albums
+    }
+  }
+export default connect(mapStateToProps, { helloWorld , addAlbum })(AlbumsListContainer)
--- src/reducers/albums.js
+++ src/reducers/albums.js
@@ -1,3 +1,32 @@
 export default (state = [], action = {}) => {
-    return state
-  }
+    switch (action.type){
+      case 'ADD_ALBUM':
+        return [
+          ...state,
+          action.payload
+        ]
+      case 'SET_ALBUM':
+        return [
+          ...state,
+          action.payload
+        ]
+      default:
+      return state
+    }
+   
+}
+
+
+
+
+  // const reducer = (state = initialState, action = {}) => {
+  //   switch (action.type) {
+  //   case 'ADD_PIZZA':
+  //   return [
+  //     ...state,
+  //     action.payload
+  //   ]
+  //   default:
+  //     return state
+  //   }
+  // }
